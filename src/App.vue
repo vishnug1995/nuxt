1a23533bbe64e322c0f3940b86b468284e205c57
vishnu riosis <vishnugriosis@gmail.com>
2022-11-16 18:29:28 +0530

    test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+    <!-- <div ref="hellow">here</div> -->
+    <!-- <div @click="adddata">add data</div> -->
+    <input v-model="name"/> <button>add data</button>
+    
+    dd
+    <ul>
+        <li>sdfgsdfg   <button>delete</button> <button>edit</button></li>
+        <li>dfgsdfg <button>delete</button> <button>edit</button></li>
+        <li>dfgsdfgsd <button>delete</button> <button>edit</button></li>
+        <li>dfgsdfg <button>delete</button> <button>edit</button></li>
+        <li>dgsdfgsdfg <button>delete</button> <button>edit</button></li>
+    </ul>
+
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {
+    mapGetters,
+    mapActions
+} from 'vuex'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data() {
+        return {
+            name: '',
+
+        }
+    },
+    computed: {
+        ...mapGetters({
+            count: 'get_count',
+        }),
+    },
+    created() {
+        console.log('created', this.name, this.$refs.hellow)
+    },
+    mounted() {
+        console.log('mounted', this.name, this.$refs.hellow)
+    },
+    methods: {
+        ...mapActions({
+            //adddata: 'increment',
+        }),
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
